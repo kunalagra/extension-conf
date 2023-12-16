8924592f6dc42f32f5bf1af12086577c41fef58e
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2023-12-16 18:33:52 +0530

    v4 Update desidime-url-tracking-protection.js

--- desidime-url-tracking-protection.js
+++ desidime-url-tracking-protection.js
@@ -1,15 +1,19 @@
 // ==UserScript==
-// @name          Desidime Redirect Removal
-// @version       3
-// @run-at        document-start
-// @description   Desidime Remove Tracking
-// @include       https://www.desidime.com/*
-// @author        MaskedWarrior
-// @license MIT
-// @namespace https://greasyfork.org/users/878597
-// @run-at       document-end
- 
+// @name        Desidime Redirect Removal
+// @version     4
+// @run-at      document-start
+// @description Remove URL Tracking/Redirects from Desidime.com
+// @include     https://www.desidime.com/*
+// @author      kunalagra
+// @license     MIT
+// @namespace   https://greasyfork.org/users/878597
+// @supportURL  https://github.com/kunalagra/
+// @run-at      document-end
+// @icon        https://cdn0.desidime.com/logo/favicon.ico
+// @downloadURL https://update.greasyfork.org/scripts/440342/Desidime%20Redirect%20Removal.user.js
+// @updateURL https://update.greasyfork.org/scripts/440342/Desidime%20Redirect%20Removal.meta.js
 // ==/UserScript==
+
 function fixDesiDimeLinks() {
     // Find and update links in anchor tags
     const anchorLinks = document.querySelectorAll("a[href*='links?ref']");
@@ -19,7 +23,7 @@ function fixDesiDimeLinks() {
             link.href = decodeURIComponent(originalURL);
         }
     });
- 
+
     // Find and update links in data-href attributes
     const dataHrefLinks = document.querySelectorAll("a[data-href*='links?ref']");
     dataHrefLinks.forEach((link) => {
@@ -30,7 +34,7 @@ function fixDesiDimeLinks() {
         }
     });
 }
- 
+
 // Function to handle DOM changes
 function handleDOMChanges(mutationsList) {
     for (const mutation of mutationsList) {
@@ -43,16 +47,15 @@ function handleDOMChanges(mutationsList) {
         }
     }
 }
- 
+
 // Wrap the MutationObserver setup inside a DOMContentLoaded event listener
 document.addEventListener('DOMContentLoaded', () => {
     // Use MutationObserver to detect changes in the document
     const observer = new MutationObserver(handleDOMChanges);
- 
+
     // Observe changes in the body and its subtree
     observer.observe(document.body, { attributes: true, childList: true, subtree: true });
- 
+
     // Call fixDesiDimeLinks once when the DOM is ready
     fixDesiDimeLinks();
 });
-
