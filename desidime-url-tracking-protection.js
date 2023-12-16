9bd16021bdbc1108e3f38c0598c3dbf5afc03825
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2023-12-16 18:33:27 +0530

    v3 of desidime-url-tracking-protection.js
    fixes FF DOM elements

--- desidime-url-tracking-protection.js
+++ desidime-url-tracking-protection.js
@@ -1,66 +1,58 @@
-    // ==UserScript==
-    // @name          Desidime Redirect Removal
-    // @version       2
-    // @run-at        document-start
-    // @description   Desidime Remove Tracking
-    // @include       https://www.desidime.com/*
-    // @author        MaskedWarrior
-    // @license MIT 
-    // @namespace https://greasyfork.org/users/878597
-    // @run-at       document-end
-     
-    // ==/UserScript==
-     
-    // Checks if page is loaded
-     
-    function DesiRM(){
-    	// loads a href which DesiDime has added tracking
-    	qLinks = document.querySelectorAll("a[href*='links?ref']")
-    	// loops through each of the a href
-    	for (var J = qLinks.length - 1; J >= 0; --J) {
-    		// Get the URL compent (href)
-    		var oldHref = qLinks[J].getAttribute('href');
-    		// Get the OriginalURL
-    		var newHref1 = oldHref.split('&url=')[1];
-    		// Deocode the URl
-    		var newHref = decodeURIComponent(newHref1)
-    		// Change the links
-    		qLinks[J].setAttribute('href', newHref);
-    	}
-    }
-    function DesiRM2(){
-    	// loads a href which DesiDime has added tracking
-    	qLinks = document.querySelectorAll("a[data-href*='links?ref']")
-    	// loops through each of the a href
-    	for (var J = qLinks.length - 1; J >= 0; --J) {
-    		// Get the URL compent (href)
-    		var oldHref = qLinks[J].getAttribute('data-href');
-    		// Get the OriginalURL
-    		var newHref1 = oldHref.split('&url=')[1];
-    		// Deocode the URl
-    		var newHref = decodeURIComponent(newHref1)
-    		// Change the links
-    		qLinks[J].setAttribute('data-href', newHref);
-        qLinks[J].setAttribute('target', '');
-     
-    	}
-    }
-     
-    document.onreadystatechange = () => {
-    	DesiRM()
-      DesiRM2()
-      
-    }
-    (new MutationObserver((mutations) => {
-        let runCheck = false
-        for (let mutation of mutations) {
-            if (mutation.addedNodes.length || mutation.attributeName === 'href') {
-                runCheck = true
-                break;
-            }
+// ==UserScript==
+// @name          Desidime Redirect Removal
+// @version       3
+// @run-at        document-start
+// @description   Desidime Remove Tracking
+// @include       https://www.desidime.com/*
+// @author        MaskedWarrior
+// @license MIT
+// @namespace https://greasyfork.org/users/878597
+// @run-at       document-end
+ 
+// ==/UserScript==
+function fixDesiDimeLinks() {
+    // Find and update links in anchor tags
+    const anchorLinks = document.querySelectorAll("a[href*='links?ref']");
+    anchorLinks.forEach((link) => {
+        const originalURL = new URL(link.href).searchParams.get('url');
+        if (originalURL) {
+            link.href = decodeURIComponent(originalURL);
         }
-        if (runCheck) {
-          DesiRM()
-          DesiRM2()
+    });
+ 
+    // Find and update links in data-href attributes
+    const dataHrefLinks = document.querySelectorAll("a[data-href*='links?ref']");
+    dataHrefLinks.forEach((link) => {
+        const originalURL = new URL(link.getAttribute('data-href')).searchParams.get('url');
+        if (originalURL) {
+            link.setAttribute('data-href', decodeURIComponent(originalURL));
+            link.removeAttribute('target'); // Remove the 'target' attribute
         }
-    })).observe(document.querySelector('body'), {attributeFilter: ['href'], childList: true, subtree: true})
+    });
+}
+ 
+// Function to handle DOM changes
+function handleDOMChanges(mutationsList) {
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'href') {
+            // Handle attribute changes (e.g., href attribute)
+            fixDesiDimeLinks();
+        } else if (mutation.type === 'childList') {
+            // Handle changes in the DOM structure (e.g., added nodes)
+            fixDesiDimeLinks();
+        }
+    }
+}
+ 
+// Wrap the MutationObserver setup inside a DOMContentLoaded event listener
+document.addEventListener('DOMContentLoaded', () => {
+    // Use MutationObserver to detect changes in the document
+    const observer = new MutationObserver(handleDOMChanges);
+ 
+    // Observe changes in the body and its subtree
+    observer.observe(document.body, { attributes: true, childList: true, subtree: true });
+ 
+    // Call fixDesiDimeLinks once when the DOM is ready
+    fixDesiDimeLinks();
+});
+
