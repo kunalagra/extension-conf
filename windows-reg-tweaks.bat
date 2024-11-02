5cfc7d9e714678648753e97f17145d49853b8930
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2024-11-02 11:24:47 +0000

    add: Unified Reg Tweaks

--- windows-reg-tweaks.bat
+++ windows-reg-tweaks.bat
@@ -1,4 +1,5 @@
 @echo off
+rem Sublime Text Tweaks
 SET st3Path=C:\Program Files\Sublime Text\sublime_text.exe
  
 rem add it for all file types
@@ -16,4 +17,13 @@ rem add it for right click on explorer
 @reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Sublime"         /t REG_SZ /v "" /d "Open with Sublime Text 3"   /f
 @reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Sublime"         /t REG_SZ /v "Icon" /d "%st3Path%" /f
 @reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\Sublime\command" /t REG_SZ /v "" /d "%st3Path% \"%%V\"" /f
+pause
+
+rem Disable Sign IN Button in Office 16
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Common\SignIn" /v "SignInOptions" /t REG_DWORD /d 3 /f
+
+rem Enable to 2 Finger Tap for Right Click (Lenovo IdeaPad)
+reg add "HKEY_CURRENT_USER\Software\Synaptics\SynTP\TouchPadRMIHID2cTM3336-1" /v "2FingerTapAction" /t REG_DWORD /d 2 /f
+
+echo Registry entries added successfully.
 pause
